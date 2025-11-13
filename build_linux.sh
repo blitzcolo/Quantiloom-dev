763748ab1176917fed7fcf09565822fd6a92e6df
wtflmao <19224718+wtflmao@users.noreply.github.com>
2025-11-13 20:54:46 +0800

    refactor: Simplify build scripts and enhance CMake configuration
    - Updated `build_linux.sh` and `build_windows.ps1` to streamline build commands and output directories.
    - Modified `CMakeLists.txt` for both `Quantiloom` and `libQuantiloom` to include shared library definitions and improved RPATH settings.
    - Added conditional post-build command for Windows to copy the DLL to the executable directory.

--- build_linux.sh
+++ build_linux.sh
@@ -1,6 +1,5 @@
-cmake -B out -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++
-cmake -B out -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++
-cmake --build out --config Debug --target libQuantiloom -j
-cmake --build out --config Release --target libQuantiloom -j
-cmake --build out --config Debug --target Quantiloom -j
-cmake --build out --config Release --target Quantiloom -j
+cmake -B out/Debug -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++
+cmake --build out/Debug -j
+
+cmake -B out/Release -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++
+cmake --build out/Release -j
--- build_windows.ps1
+++ build_windows.ps1
@@ -1,4 +1,4 @@
-cmake -B build -G "Visual Studio 18 2026" -A x64 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_GENERATOR_PLATFORM=x64
-cmake -B build -G "Visual Studio 18 2026" -A x64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_GENERATOR_PLATFORM=x64 
-cmake --build build --config Debug --target ALL_BUILD -j
-cmake --build build --config Release --target ALL_BUILD -j
+cmake -B build -G "Visual Studio 18 2026" -A x64
+
+cmake --build build --config Debug -j
+cmake --build build --config Release -j
