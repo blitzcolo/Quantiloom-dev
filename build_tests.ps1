3b46ee893199a337f1cae5cefae481c54b0e03e3
wtflmao <19224718+wtflmao@users.noreply.github.com>
2025-12-04 10:13:09 +0800

    refactor: streamline test build scripts and enhance CMake configuration
    - Combined multiple commands in `build_tests.ps1` and `build_tests.sh` for improved readability and efficiency.
    - Updated `CMakeLists.txt` to include additional test sources for core, scene, and I/O modules, enhancing test coverage.
    
    These changes simplify the testing process and expand the test suite, contributing to better code quality and maintainability.

--- build_tests.ps1
+++ build_tests.ps1
@@ -1,5 +1,4 @@
 New-Item -ItemType Directory -Force -Path build
 Set-Location build
-cmake .. -DBUILD_TESTING=ON -DENABLE_COVERAGE=ON
-cmake --build . --target libquantiloom_tests -j
-.\tests\Debug\libquantiloom_tests.exe
+cmake .. -DBUILD_TESTING=ON -DENABLE_COVERAGE=ON && cmake --build . --target libquantiloom_tests -j && .\tests\Debug\libquantiloom_tests.exe
+Set-Location ..
--- build_tests.sh
+++ build_tests.sh
@@ -1,4 +1,3 @@
 cd out
-cmake .. -DBUILD_TESTING=ON -DENABLE_COVERAGE=ON
-LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH cmake --build . --target libquantiloom_tests -j
-LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH ./tests/libquantiloom_tests
+cmake .. -DBUILD_TESTING=ON -DENABLE_COVERAGE=ON && LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH cmake --build . --target libquantiloom_tests -j && LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH ./tests/libquantiloom_tests
+cd ..
