49fd1ce7e5ace62d34673c1c3fe3de0dbfd7030f
wtflmao <19224718+wtflmao@users.noreply.github.com>
2025-12-14 15:27:26 +0800

    chore: update build scripts and .gitignore for test directories
    - Modified `.gitignore` to include new test build directories: `build-test/` and `out-test/`.
    - Updated `build_tests.ps1` to create and navigate to `build-test` instead of `build`.
    - Adjusted `build_tests.sh` to create `out-test` and change directory to it for test builds.
    
    These changes improve the organization of test builds and ensure that temporary files are properly ignored.

--- build_tests.ps1
+++ build_tests.ps1
@@ -1,4 +1,4 @@
 New-Item -ItemType Directory -Force -Path build
-Set-Location build
+Set-Location build-test
 cmake .. -DBUILD_TESTING=ON -DENABLE_COVERAGE=ON && cmake --build . --target libquantiloom_tests -j && .\tests\Debug\libquantiloom_tests.exe
 Set-Location ..
--- build_tests.sh
+++ build_tests.sh
@@ -1,3 +1,4 @@
-cd out
+mkdir out-test 
+cd out-test
 LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH cmake .. -DBUILD_TESTING=ON -DENABLE_COVERAGE=ON && LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH cmake --build . --target libquantiloom_tests -j && LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH ./tests/libquantiloom_tests
 cd ..
