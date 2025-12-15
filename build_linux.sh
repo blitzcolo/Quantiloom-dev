090145c12aef835d579e75d8462a745dcbfd4511
wtflmao <19224718+wtflmao@users.noreply.github.com>
2025-12-16 04:49:02 +0800

    chore: update build scripts to disable test builds by default
    - Modified `build_linux.sh` and `build_windows.ps1` to include the `-DQUANTILOOM_BUILD_TESTS=OFF` flag in the CMake commands, preventing test builds during the release process.
    - These changes streamline the build process for production environments, ensuring that test-related configurations are not included by default.

--- build_linux.sh
+++ build_linux.sh
@@ -2,5 +2,5 @@
 #cmake -B out/Debug -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++
 #cmake --build out/Debug -j
 
-cmake -B out/Release -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++
+cmake -B out/Release -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++ -DQUANTILOOM_BUILD_TESTS=OFF
 LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH cmake --build out/Release -j
--- build_windows.ps1
+++ build_windows.ps1
@@ -1,5 +1,5 @@
 ./src/shaders/compile_shaders.bat
-cmake -B build -G "Visual Studio 18 2026" -A x64
+cmake -B build -G "Visual Studio 18 2026" -A x64 -DQUANTILOOM_BUILD_TESTS=OFF
 
 #cmake --build build --config Debug -j
 cmake --build build --config Release -j
